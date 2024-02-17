453fd5c1f7deb6c600f9451407db1469abd77d6a
Todd Schluechtermann <dyson_swarm@outlook.com>
2024-02-16 21:59:22 -0600

    Adding styles

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,10 +1,12 @@
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans"
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,11 +20,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}      
-        <Analytics />
-        <SpeedInsights />
-      </body>
+      <body 
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.variable
+        )}>{children}</body>
     </html>
   );
 }
